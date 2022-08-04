2aca9ebe843eccc2eda76c74737ad3d2692957d7
Valeria Vitvytska <leranmu@gmail.com>
2022-08-04 09:01:59 +0300

    feat:update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import Card from "./classes/card.js";
-import Modal from "./classes/modal.js";
 import createCard from "./classes/createcard.js";
 import Login from "./classes/login.js";
-import Visit from "./classes/visit.js";
-import instance from "./fuctions/instance.js";
+
