f1d498c9c42720a23cf29709ce122e66dd44e5bc
Valeria Vitvytska <leranmu@gmail.com>
2022-08-09 18:53:10 +0000

    Update login.js

--- classes/login.js
+++ classes/login.js
@@ -93,23 +93,7 @@ class Login extends Modal{
           login()
 
 
-           /* fetch("https://ajax.test-danit.com/api/v2/cards/login", {
-                method: 'POST',
-                 headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                 
-                    email: input[0].value,
-                   password: input[1].value,
-                }),
-               
-            })
-                .then(response => response.text())
-                .then(token =>{ console.log(token)
-                localStorage.setItem('token',token)
-                }
-                )*/
+          
          
       
           
