83e245e4078cb347863916a8f60bb9e1dc3eb1f6
Valeria Vitvytska <leranmu@gmail.com>
2022-08-09 21:49:05 +0300

    feat:update index.js index.html

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 import Card from "./classes/card.js";
+
 import createCard from "./classes/createcard.js";
 import Login from "./classes/login.js";
+import getUrgency from "./functions/getUrgency.js";
 import createLogin from "./functions/createLogin.js";
+import createCards from "./functions/createCards.js";
+import renderCardsAfterReload from "./functions/renderCardsAfterReload.js";
+import filterCards from "./functions/filterFuction.js";
+
+
+
+
+
+const container = document.querySelector('.container')
 
 
 
 const loginBtn = document.querySelector('.login_btn')
-/*function createCards(){
-new createCard().render('.container')
-    
-}
 
-function createLogin(){
-
-
-   const login = new Login()
-
- // loginBtn.removeEventListener('click',createLogin)
-login.render('.container')
-
-login.submitLogin() 
-
-//loginBtn.addEventListener('click',createCards)
-}
-*/
 loginBtn.addEventListener('click', createLogin)
 
 
+//new Card('Иван Иванов','терапевт','обследование','боль в желудке','срочно').render('.container')
 
 const urgencySelect = document.querySelector('.visit_select')
-
 
 
 
@@ -40,17 +34,18 @@ if(localStorage.getItem('token')){
   logOutBtn.classList.add('logout_btn')
   const header = document.querySelector('header')
   header.append(logOutBtn)
-logOutBtn.addEventListener('click',() =>{
+
+ logOutBtn.addEventListener('click',() =>{
+
+  
 
   localStorage.clear()
   document.location.reload()
  
 })
-   // renderCardsAfterReload()
+
+
+    renderCardsAfterReload()
 }
-
-
-
  
-//const logOutBtn = document.querySelector('.logout_btn')
 
