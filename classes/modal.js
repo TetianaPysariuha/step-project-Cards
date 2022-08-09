0032eb4ec34bac1f65e408a094c4225f50a44f4f
Tetiana Pysariuha <tetyanka.p.tv@gmail.com>
2022-08-09 15:20:52 +0300

    feat: class Modal created

--- classes/modal.js
+++ classes/modal.js
@@ -1,9 +1,30 @@
 class Modal{
+    constructor(submitFuncton) {
+        this._modalElement = document.createElement('div');
+        this._modalBackgroundContainer = document.createElement('div');
+        this._modalMainContainer = document.createElement('div');
+        this._modalCloseBtn = document.createElement('button');
+    }
 
+    createNode() {
+        this._modalElement.className = 'modal';
+        this._modalBackgroundContainer.className = 'modal__background';
+        this._modalMainContainer.className = 'modal__main-container flex-column';
+        this._modalCloseBtn.className = 'modal__close-btn flex-column';
+        this._modalCloseBtn.innerText = 'X';
+        this._modalCloseBtn.addEventListener('click', this.deleteNode.bind(this));
+        this._modalBackgroundContainer.append(this._modalMainContainer);
+        this._modalElement.append(this._modalBackgroundContainer);
+    }
 
+    deleteNode(){
+        this._modalElement.remove();
+    }
 
-
-
+    render(){
+        this.createNode();
+        document.body.append(this._modalElement);
+    }
 }
 
 
