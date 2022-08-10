4cf317e657419f465f83404e1b8172695e6c9f3b
Tetiana Pysariuha <tetyanka.p.tv@gmail.com>
2022-08-10 19:08:23 +0300

    feat: class visitDantist and visitTherapist

--- classes/visitDentist.js
+++ classes/visitDentist.js
@@ -1,9 +1,27 @@
-import Visit from "./visit.js";
+import Visit from './visit.js'
+import postNewCards from '../functions/postCardFunc.js';
 
-class visitDentist extends Visit{
+class VisitDentist extends Visit{
+    constructor() {
+        super();
+    }
 
+    createElements() {
+        super.createElements();
+        this.form.insertAdjacentHTML('beforeend', `<label for="visit-date">Дата последнего посещения</label>
+        <input id="visit-date" type="date">`);
+        this.submitBtn.addEventListener('click', this.postCard.bind(this));
+        this.form.append(this.submitBtn);
+    }
 
-
+    postCard() {
+        const urgencySelector = document.querySelector('.visit_select');
+        postNewCards({doctor: 'Dantist',
+                        name: document.querySelector('#name').value,
+                        purpose: document.querySelector('#purpose').value,
+                        description: document.querySelector('#description').value,
+                        urgency: urgencySelector.options[urgencySelector.selectedIndex].text,
+                        lastVisitDate: document.querySelector('#visit-date').value});
+        this.closeModal();
+    }
 }
-
-export default visitDentist;
--- classes/visitTherapist.js
+++ classes/visitTherapist.js
@@ -1,10 +1,39 @@
-import Visit from "./visit.js";
+import Visit from './visit.js'
+import postNewCards from '../functions/postCardFunc.js';
 
-class visitTherapist extends Visit{
+class VisitTherapist extends Visit{
+    constructor() {
+        super();
+    }
 
+    createElements() {
+        super.createElements();
+        this.form.insertAdjacentHTML('beforeend', `<label for="pressure">Обычное давление</label>
+        <input id="pressure" type="text">
+        <label for="mass-body-index">Индекс массы тела</label>
+        <input id="mass-body-index" type="text">
+        <label for="heart-diseases">Перенесенные заболевания сердечно-сосудистой системы</label>
+        <textarea id="heart-diseases" rows="10"></textarea>
+        <label for="age">Возраст</label>
+        <input id="age" type="number">`);
+        this.submitBtn.addEventListener('click', this.postCard.bind(this));
+        this.form.append(this.submitBtn);
+        this.form.append()
+    }
 
-
-
+    postCard() {
+        debugger;
+        const urgencySelector = document.querySelector('.visit_select');
+        postNewCards({doctor:'Therapist',
+                        name:document.querySelector('#name').value,
+                        purpose:document.querySelector('#purpose').value,
+                        description:document.querySelector('#description').value,
+                        urgency: urgencySelector.options[urgencySelector.selectedIndex].text,
+                        pressure:document.querySelector('#pressure').value,
+                        massBodyIndex:document.querySelector("#mass-body-index").value,
+                        heartDiseases:document.querySelector("#heart-diseases").value,
+                        age:document.querySelector("#age").value});
+    }
 }
 
-export default visitTherapist;
+export default VisitTherapist;
