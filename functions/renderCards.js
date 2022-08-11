d0410e91c51bf9f08771999c2c228da52139c643
Valeria Vitvytska <leranmu@gmail.com>
2022-08-11 15:01:36 +0300

    feat:create CardforCardiologist update visitCardio

--- functions/renderCards.js
+++ functions/renderCards.js
@@ -2,6 +2,9 @@ import getCards from "./getCards.js";
 import Card from "../classes/card.js";
 import Filter from "../classes/filter.js";
 import filterCards from "./filterFuction.js";
+import CardforCardiologist from "../classes/cardforCardiologist.js";
+import CardforDentist from "../classes/cardforDentist.js";
+import CardforTherapist from "../classes/cardforTherapist.js";
 
 
 const renderCards = ( )=>{
@@ -20,17 +23,22 @@ const renderCards = ( )=>{
 
  cardsArr.then(result =>{ console.log(result)
 
-if(result === []){
-  const titleForBegin =document.createElement('h1')
-  document.querySelector('.container').append( titleForBegin)
-  document.querySelector('h1').innerHTML = 'There is no items added'
-}
+
 
 result.forEach(el =>{
+       if(el.doctor === 'cardiologist'){
+       new CardforCardiologist(el.id,el.name,el.doctor,el.purpose,el.description,el.urgency,el.pressure,el.weight,el.age,'заболевания').render('.container')}
+      else if(el.doctor ==='dentist'){
+        new CardforDentist(el.id,el.name,el.doctor,el.purpose,el.description,el.urgency,el.lastVisitDate).render('.container')
+      }
+      else{
 
-    if(el.doctor === 'cardiologist'){
-       new Card(el.id,el.name,el.doctor,el.purpose,el.description,el.urgency).render('.container')
-    }})
+        new CardforTherapist(el.id,el.name,el.doctor,el.purpose,el.description,el.urgency,el.age).render('.container') 
+      }
+      
+      
+      }
+    )
 
 
 
