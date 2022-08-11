f87f41ee66fbf4dbbf96013633e3fd17df3eb758
Tetiana Pysariuha <tetyanka.p.tv@gmail.com>
2022-08-11 19:38:21 +0300

    fix: delete element from storage

--- functions/deleteCardStorage.js
+++ functions/deleteCardStorage.js
@@ -1,6 +1,6 @@
 function deleteCardStorage(id) {
     const cards = JSON.parse(localStorage.getItem('cards'));
-    cards.splice(cards.indexOf((element) => element.id === id));
+    cards.splice(cards.indexOf((element) => element.id === id), 1);
     localStorage.setItem('cards', JSON.stringify(cards));
 }
 
