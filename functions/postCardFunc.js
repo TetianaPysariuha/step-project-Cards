d5d3d4d6825a88c94bf62888523e1ceb14e844c8
Valeria Vitvytska <leranmu@gmail.com>
2022-08-11 22:41:30 +0300

    feat:update postCardFunc create new func

--- functions/postCardFunc.js
+++ functions/postCardFunc.js
@@ -1,6 +1,7 @@
 import Card from "../classes/card.js"
 import instance from "./instance.js"
 import renderAfterReloadwithAddedCard from "./renderCardsAfterAddNewCardAndReload.js"
+import renderCardForDifferentDoctors from "./renderForDifferentDoctors.js"
 
 
 const postNewCards = async (obj) =>{
@@ -8,8 +9,10 @@ const postNewCards = async (obj) =>{
   
     const {status,data} =  await instance.post('',obj)
        console.log(data)
-      const newCard = new Card(data.id,data.name,data.doctor,data.purpose,data.description,data.urgency)
-      newCard.render('.container')
+     // const newCard = new Card(data.id,data.name,data.doctor,data.purpose,data.description,data.urgency)
+     // newCard.render('.container')
+        renderCardForDifferentDoctors(data)
+
       localStorage.setItem('newcard',JSON.stringify(data))
           
   renderAfterReloadwithAddedCard() 
