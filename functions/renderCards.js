7671d1e57ec351b36a05d40dc33704542573ae84
Valeria Vitvytska <leranmu@gmail.com>
2022-08-12 20:49:10 +0300

    feat:update renderCards

--- functions/renderCards.js
+++ functions/renderCards.js
@@ -23,7 +23,7 @@ const renderCards = () => {
       if (el.doctor === 'cardiologist') {
         new CardforCardiologist(el.id, el.name, el.doctor, el.purpose, el.description, el.urgency, el.pressure, el.weight, el.age,el.disease ).render('.container')
       }
-      else if (el.doctor === 'Dantist') {
+      else if (el.doctor === 'dentist') {
         new CardforDentist(el.id, el.name, el.doctor, el.purpose, el.description, el.urgency, el.lastVisitDate).render('.container')
       }
       else {
