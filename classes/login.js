7c7ce6a92983c39643642ddfc258d2b4fd66f865
Valeria Vitvytska <leranmu@gmail.com>
2022-08-12 10:20:59 +0300

    feat:update Login

--- classes/login.js
+++ classes/login.js
@@ -7,34 +7,34 @@ import createCards from "../functions/createCards.js";
 import Card from "./card.js";
 
 
-class Login extends Modal {
-    constructor() {
-        super()
-        this.emailInput = document.createElement('input')
-        this.emailInputLabel = document.createElement('label')
-        this.passwordInput = document.createElement('input')
-        this.passwordInputLabel = document.createElement('label')
-        this._submitButton = document.createElement('input')
-        this.form = document.createElement('form')
+class Login extends Modal{
+    constructor(){
+     super()   
+     this.emailInput = document.createElement('input')
+     this.emailInputLabel = document.createElement('label')
+     this.passwordInput = document.createElement('input')
+     this.passwordInputLabel = document.createElement('label')
+     this._submitButton = document.createElement('input')
+     this.form = document.createElement('form')
     }
-    createElements() {
+    createElements(){
         super.createElements()
         this.passwordInput.type = 'text'
         this.emailInput.type = 'text'
         this.passwordInput.id = 'password'
         this.emailInput.id = 'email'
         this.passwordInputLabel.for = 'password'
-        this.emailInputLabel.innerHTML = 'Email'
+        this.emailInputLabel.innerHTML='Email'
         this.emailInputLabel.for = 'email'
-        this.passwordInputLabel.innerHTML = 'Password'
+        this.passwordInputLabel.innerHTML='Password'
         this._submitButton.type = 'submit'
         this._submitButton.value = 'Submit'
         this._mainContainer.append(this.form)
-        this.form.append(this.emailInputLabel)
+       this.form.append(this.emailInputLabel)
         this.form.append(this.emailInput)
-        this.form.append(this.passwordInputLabel)
-        this.form.append(this.passwordInput)
-        this.form.append(this._submitButton)
+       this.form.append(this.passwordInputLabel)
+       this.form.append(this.passwordInput)
+       this.form.append(this._submitButton)
 
     }
     submitLogin() {
@@ -51,55 +51,76 @@ class Login extends Modal {
 
 
 
-            const login = async () => {
-                try {
-                    const { status, data } = await instance.post('/login', { email: document.querySelector('#email').value, password: document.querySelector('#password').value })
-                    console.log(data)
-                    localStorage.setItem('token', data)
-                    renderCards()
+            const login = async () =>{
+                try{
+             const {status,data} =  await instance.post('/login',{email:document.querySelector('#email').value,password:document.querySelector('#password').value})
+                console.log(data)
+              localStorage.setItem('token',data)
+                  renderCards() 
+                
+                
+         this.closeModal()
+          const loginBtn = document.querySelector('.login_btn')
 
+          loginBtn.innerHTML = 'Cоздать новую карту'
+          loginBtn.removeEventListener('click',createLogin)
 
-                    this.closeModal()
-                    const loginBtn = document.querySelector('.login_btn')
-
-                    loginBtn.innerHTML = 'Cоздать новую карту'
-                    loginBtn.removeEventListener('click', createLogin)
-
-                    loginBtn.addEventListener('click', createCards)
-                    const logOutBtn = document.createElement('button')
-                    logOutBtn.innerHTML = 'Выйти'
-                    logOutBtn.classList.add('logout_btn')
-                    const header = document.querySelector('header')
-                    header.append(logOutBtn)
-
-                    logOutBtn.addEventListener('click', () => {
-
-
-
-                        localStorage.clear()
-                        document.location.reload()
-
-                    })
-
-
-
+         loginBtn.addEventListener('click',createCards)
+         const logOutBtn = document.createElement('button')
+         logOutBtn.innerHTML = 'Выйти'
+         logOutBtn.classList.add('logout_btn')
+         const header = document.querySelector('header')
+         header.append(logOutBtn)
+       
+        logOutBtn.addEventListener('click',() =>{
+       
+         
+       
+         localStorage.clear()
+         document.location.reload()
+        
+       })
+             
+                
+                
                 }
-                catch (err) {
-                    console.log(err)
-                    alert('Invalid email or password')
-                }
+                  catch(err){
+                      console.log(err)
+                      alert('Invalid email or password')
+                  }
             }
 
-            login()
+          login()
 
 
+           /* fetch("https://ajax.test-danit.com/api/v2/cards/login", {
+                method: 'POST',
+                 headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                 
+                    email: input[0].value,
+                   password: input[1].value,
+                }),
+               
+            })
+                .then(response => response.text())
+                .then(token =>{ console.log(token)
+                localStorage.setItem('token',token)
+                }
+                )*/
+         
+      
+          
+         
         })
 
 
     }
-    render(selector) {
+    render(selector){
         this.createElements();
-
+    
         document.querySelector(selector).append(this._modalElement);
     }
 
