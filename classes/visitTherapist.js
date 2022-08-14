b9a81fd14ba756b21e3ae91f63811b21d76eade3
Valeria Vitvytska <leranmu@gmail.com>
2022-08-14 13:52:47 +0300

    delete visitTherapist

--- classes/visitTherapist.js
+++ classes/visitTherapist.js
@@ -1,28 +1,7 @@
 import Visit from "./visit.js";
 
 class VisitTherapist extends Visit{
-    constructor(){
-        super()
-       
-    }
-    createElements(){
-        super.createElements()
-       
-       this.form.insertAdjacentHTML('beforeend',`
-       <label for="pressure">Возраст</label><br>
-       <input id ="pressure" type ="text" value=""><br>
-      
-       `)
-       this.submitBtn.addEventListener('click',()=>{
-
-
-        
-    })
-    }
-    render(selector){
-        this.createElements()
-        document.querySelector(selector).append(this.form);
-    }
+   
     
     }
     
