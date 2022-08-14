eea2fef5c3748c54262a787dc2e390a930a03299
Valeria Vitvytska <leranmu@gmail.com>
2022-08-14 07:43:21 +0300

    update cardforDentist card for Therapist

--- classes/cardforTherapist.js
+++ classes/cardforTherapist.js
@@ -2,36 +2,49 @@ import Card from "./card.js";
 
 import instance from "../functions/instance.js";
 
-class CardforTherapist extends Card{
+class CardforTherapist extends Card {
 
-constructor(id,name,doctor,purpose,description,urgency,age){
+    constructor(id, name, doctor, purpose, description, urgency, age) {
 
-    super(id,name,doctor,purpose,description,urgency)
-    this._age= age;
+        super(id, name, doctor, purpose, description, urgency)
+        this._age = age;
 
 
-}
+    }
 
-createElements(){
-super.createElements();
-this.ul.insertAdjacentHTML('beforeend',`
-<li><span>Дата последнего визита:</span>${this._age}</li>
+    createElements() {
+        super.createElements();
+       
 
+
+this.showMoreBtn.addEventListener('click',()=>{
+
+    const showMore = document.querySelector('.show_more')
+    if(showMore){showMore.remove()
+     this.ul.innerHTML = ''   
+    }
+    this.cardContainer.append(this.ul)
+    
+       this.ul.insertAdjacentHTML('beforeend',`
+    
+    <li><span>Цель визита:</span>${this._purpose}</li>
+    <li><span>Краткое описание визита:</span>${this._description}</li>
+    <li><span>Срочность:</span>${this._urgency}</li>
+    <li><span>Возраст:</span>${this._age}</li>
 `)
+    
+        
+       document.querySelectorAll('.card_container').forEach(el =>el.style.height = '170px' )
+    this.cardContainer.style.height = '300px'
+    })
+
+    }
+    render(selector) {
+        this.createElements()
 
 
-
-
-
-}
-render(selector){
-    this.createElements()
-
-
-document.querySelector(selector).append(this.cardContainer)
-}
-
-
+        document.querySelector(selector).append(this.cardContainer)
+    }
 
 }
 
